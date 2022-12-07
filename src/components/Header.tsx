177337e66545dc2bce7f1a9d4152167c02289217
carlos <carl.devfs@gmail.com>
2022-12-07 11:28:43 +0100

    add: create header

---
+++ src/components/Header.tsx
@@ -0,0 +1,30 @@
+import "./Header.scss";
+
+const Header = () => {
+  return (
+    <header className="header">
+      <div className="logo-container">
+        <img src="./logo.png" alt="Logo" />
+      </div>
+      <nav>
+        <ul>
+          <li>
+            <a href="#">Accueil</a>
+          </li>
+          <li>
+            <a href="#">Profil</a>
+          </li>
+          <li>
+            <a href="#">Réglage</a>
+          </li>
+          <li>
+            <a href="#">Communauté</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
+/** Created by carlos on 30/11/2022 */
