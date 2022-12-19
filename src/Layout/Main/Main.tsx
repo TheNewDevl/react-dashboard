1a5a66e3d1dae1b4624c75fb0e32f41a98b051ef
carlos <carl.devfs@gmail.com>
2022-12-19 15:55:23 +0100

    add : make vertical layout responsive

--- src/Layout/Main/Main.tsx
+++ src/Layout/Main/Main.tsx
@@ -23,8 +23,20 @@ const Main = ({ children }: MainProps) => {
     /** set main margin-top = header height */
     const handleMarginTop = () => {
       const header = document.querySelector("header");
+      const verticalLayout = document.querySelector(".VerticalLayout");
+
       const height = header?.clientHeight;
       mainRef.current?.style.setProperty("margin-top", `${height}px`);
+      mainRef.current?.style.setProperty(
+        "margin-left",
+        `${verticalLayout?.getBoundingClientRect().y === 0 ? verticalLayout?.clientWidth : 0}px`
+      );
+      mainRef.current?.style.setProperty(
+        "width",
+        `calc(100vw - ${
+          verticalLayout?.getBoundingClientRect().x === 0 ? verticalLayout?.clientWidth : 0
+        }) `
+      );
     };
     handleMarginTop();
     const handleResize = () => !isResizing && setIsResizing(true);
