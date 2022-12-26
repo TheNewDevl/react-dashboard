cc7c1d91aacf16c72dfd6593cb016a98ec6d6e46
carlos <carl.devfs@gmail.com>
2022-12-26 23:58:13 +0100

    add : Loader

---
+++ src/components/Loader/Loader.tsx
@@ -0,0 +1,9 @@
+import style from "./Loader.module.scss";
+
+interface LoaderProps {}
+
+export const Loader = ({}: LoaderProps) => {
+  return <div className={style.Loader}></div>;
+};
+
+/** Created by carlos on 26/12/2022 */
