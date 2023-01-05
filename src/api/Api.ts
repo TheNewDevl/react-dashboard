b57b1f6bcb4a42c62c90fe1eb7875f22e8651264
carlos <carl.devfs@gmail.com>
2023-01-05 10:27:23 +0100

    improve types and docs

--- src/api/Api.ts
+++ src/api/Api.ts
@@ -1,39 +1,48 @@
 import { DisplayData } from "../models/DisplayData";
-import { ActivityData, AverageDay, PerformanceData, User } from "../utils/types/types";
+import {
+  ActivityResponse,
+  AverageResponse,
+  GetMethodReturns,
+  PerformanceResponse,
+  UserResponse
+} from "../utils/types/types";
 
 /**
- * @description This class is used to fetch data from a REST API
+ * @description REST API calls (get, post..)
  */
 export class Api {
   private readonly _baseUrl: string;
 
+  /** @param {string} url - The base url of the API */
   constructor(url: string) {
     this._baseUrl = url;
   }
 
   /**
+   * Return data or throw an error if the response is not ok
    * @param {string} path - The uri path to fetch
    * @param {Headers} headers - The headers to send with the request
-   * @return {Promise<any>} - The response from the API
+   * @return {Promise<any> | undefined} - The response from the API
    */
-  async get(path: string, headers: Headers): Promise<any> {
+  async get(path: string, headers: Headers): Promise<GetMethodReturns> {
     return await this._handleResponse(path, { headers, method: "GET" });
   }
 
-  async delete(path: string, headers: Headers): Promise<any> {
+  async delete(path: string, headers: Headers): Promise<Response> {
     return this._handleResponse(path, { headers, method: "delete" });
   }
 
-  async post(path: string, headers: Headers, data: BodyInit): Promise<any> {
+  async post(path: string, headers: Headers, data: BodyInit): Promise<Response> {
     return this._handleResponse(path, { headers, method: "post", body: data });
   }
 
-  async patch(path: string, headers: Headers, data: BodyInit): Promise<any> {
+  async patch(path: string, headers: Headers, data: BodyInit): Promise<Response> {
     return this._handleResponse(path, { headers, method: "patch", body: data });
   }
 
   /**
    * Handle the response from the API and convert it to JSON if possible
+   * Throw an error if the response is not ok
    * @param {string} path - The uri path to fetch
    * @param {RequestInit} requestInit - The request options (method, headers, body...)
    * @return {Promise<any>}
@@ -51,8 +60,8 @@ export class Api {
 }
 
 /**
- * @description This class is used to fetch data from the Rest API
- * It will contain the different methods and endpoints to fetch data that will be used in the app
+ * @description This class is used consume the REST API class
+ * Methods represent the different endpoints of the API.
  */
 class Store {
   private readonly _api: Api;
@@ -65,42 +74,34 @@ class Store {
 
   /**
    * @param {string} userId - The user id to be used in the request
-   * @return {Promise<AverageDay[]>} - Data for the Line chart
+   * @return {Promise<AverageResponse>} - Data for the Line chart
    */
-  async averageSessions(userId: string): Promise<any> {
-    return await this._api
-      .get(`/user/${userId}/average-sessions`, new Headers())
-      .then((data) => data);
+  async averageSessions(userId: string): Promise<AverageResponse> {
+    return await this._api.get(`/user/${userId}/average-sessions`, new Headers())
   }
 
   /**
    * @param {string} userId - The user id to be used in the request
-   * @return {Promise<PerformanceData[]>} - Data for the Radar chart
+   * @return {Promise<PerformanceResponse>} - Data for the Radar chart
    */
-  async performance(userId: string): Promise<any> {
-    return await this._api
-      .get(`/user/${userId}/performance`, new Headers())
-      .then((data) => data);
+  async performance(userId: string): Promise<PerformanceResponse> {
+    return await this._api.get(`/user/${userId}/performance`, new Headers())
   }
 
   /**
    * @param {string} userId - The user id to be used in the request
-   * @return {Promise<ActivityData[]>} - Data for the Bar chart
+   * @return {Promise<ActivityResponse>} - Data for the Bar chart
    */
-  async activity(userId: string): Promise<any> {
-    return await this._api
-      .get(`/user/${userId}/activity`, new Headers())
-      .then((data) => data);
+  async activity(userId: string): Promise<ActivityResponse> {
+    return await this._api.get(`/user/${userId}/activity`, new Headers())
   }
 
   /**
    * @param {string} userId - The user id to be used in the request
-   * @return {Promise<User>} - User first name, data for radial chart and key data
+   * @return {Promise<UserResponse>} - User first name, data for radial chart and key data
    */
-  async user(userId: string): Promise<any> {
-    return await this._api
-      .get(`/user/${userId}`, new Headers())
-      .then((data) => data);
+  async user(userId: string): Promise<UserResponse> {
+    return await this._api.get(`/user/${userId}`, new Headers())
   }
 }
 
--- src/utils/types/types.ts
+++ src/utils/types/types.ts
@@ -53,3 +53,34 @@ export type AllFormattedData = {
   perfData: PerformanceData[];
   averageSessions: AverageDay[];
 }
+
+export interface AverageResponse {
+  data: {
+    userId: number;
+    sessions: { day: number, sessionLength: number }[];
+  };
+}
+export interface ActivityResponse {
+  data: {
+    sessions : { day: string, kilogram: number, calories: number }[];
+  }
+}
+
+export interface PerformanceResponse {
+  data: {
+    data: {value: number, kind: number}[];
+    kind: {[key: string]: string};
+  }
+}
+
+export interface UserResponse {
+ data: {
+   id: number;
+   keyData: {calorieCount: number, carbohydrateCount: number, lipidCount: number, proteinCount: number};
+   score?: number;
+   todayScore?: number;
+   userInfos: {firstName: string, lastName: string, age: number};
+ }
+}
+
+export type GetMethodReturns = AverageResponse & PerformanceResponse & ActivityResponse& UserResponse
